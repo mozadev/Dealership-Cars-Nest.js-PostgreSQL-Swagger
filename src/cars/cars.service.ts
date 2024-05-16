ebfb120a1f88be5c76b419ab889e60fb4a86e6c5
ceosmore <ceosmore@gmail.com>
2024-05-16 16:34:36 -0500

    feat: Update createCar method in CarsController to use carsService for creating a new car

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Car } from './interfaces/car.interfaces';
 import { v4 as uuid } from 'uuid';
+import { CreateCarDto } from './dto/create-car.dto';
 
 // import interface car to use it in the service, this advice us to use the same interface in the service and the controller
 @Injectable()
@@ -31,5 +32,14 @@ export class CarsService {
     const car = this.cars.find((car) => car.id === id);
     if (!car) throw new NotFoundException(`Car with id ${id} not found`);
     return car;
+  }
+
+  create(createCarDto: CreateCarDto) {
+    const newCar: Car = {
+      id: uuid(),
+      ...createCarDto,
+    };
+    this.cars.push(newCar);
+    return newCar;
   }
 }
