39337c3fe51b9ba2471f1af742d29be0ba796665
ceosmore <ceosmore@gmail.com>
2024-05-16 12:30:00 -0500

    feat: Update global validation pipe in main to use forbidNonWhitelisted: true to throw bad request if there is properties that are not in the Dto

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // remove the properties that are not in the Dto
-      // forbidNonWhitelisted: true, // lanza bad request si hay propiedades que no estan en el Dto
+      forbidNonWhitelisted: true, // lanza bad request si hay propiedades que no estan en el Dto
     }),
   ); // validate Dtos, This pipe is used to validate the data that comes from the body of the request and the parameters of the request, apply the validation to all the methods in the controller
 
