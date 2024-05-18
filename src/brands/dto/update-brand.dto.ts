2383034e745165c7dd43c6e6f1a05cb9137cc515
ceosmore <ceosmore@gmail.com>
2024-05-17 19:00:18 -0500

    feat: Update UpdateBrandDto to extend PartialType(CreateBrandDto)
    ```

--- src/brands/dto/update-brand.dto.ts
+++ src/brands/dto/update-brand.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBrandDto } from './create-brand.dto';
 
+// partial type is used to make all the properties in the CreateBrandDto optional
 export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
+
+// export class UpdateBrandDto {}
