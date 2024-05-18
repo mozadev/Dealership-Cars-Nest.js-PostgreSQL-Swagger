749afe466a90503dbb15db723a58f273cce5e237
ceosmore <ceosmore@gmail.com>
2024-05-17 23:23:34 -0500

    feat: Implement update method  using updateBrandDto

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -42,11 +42,20 @@ export class BrandsService {
     return brand;
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`;
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brandDB = this.findOne(id);
+
+    this.brands = this.brands.map((brand) => {
+      if (brand.id === id) {
+        brandDB.updatedAt = new Date().getTime();
+        brand = { ...brand, ...updateBrandDto };
+        return brandDB;
+      }
+      return brand;
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} brand`;
+    // return `This action removes a #${id} brand`;
   }
 }
