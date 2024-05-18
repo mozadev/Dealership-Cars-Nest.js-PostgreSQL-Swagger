948844745f809d5d8753419d69da745412935182
ceosmore <ceosmore@gmail.com>
2024-05-17 22:01:51 -0500

    feat: Update UpdateBrandDto to include name validation

--- src/brands/dto/update-brand.dto.ts
+++ src/brands/dto/update-brand.dto.ts
@@ -1,7 +1,14 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBrandDto } from './create-brand.dto';
+// import { PartialType } from '@nestjs/mapped-types';
+// import { CreateBrandDto } from './create-brand.dto';
+
+import { IsString, MinLength } from 'class-validator';
 
 // partial type is used to make all the properties in the CreateBrandDto optional
-export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
+// export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
+// This will make more sense when CreattebrandDto will have more properties
 
-// export class UpdateBrandDto {}
+export class UpdateBrandDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
