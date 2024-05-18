35c537b6e8fb3e442894479b34ec6ca599fc4649
ceosmore <ceosmore@gmail.com>
2024-05-17 21:26:35 -0500

    feat: Add initial brand to BrandsService

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+import { Brand } from './entities/brand.entity';
+
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
 
 @Injectable()
 export class BrandsService {
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: new Date().getTime(),
+    },
+  ];
+
   create(createBrandDto: CreateBrandDto) {
     return 'This action adds a new brand';
   }
