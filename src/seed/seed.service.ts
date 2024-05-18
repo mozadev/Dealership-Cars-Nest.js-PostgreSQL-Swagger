74c0ef18654e30d185de8d90a1793e8e1a2db340
ceosmore <ceosmore@gmail.com>
2024-05-18 14:33:02 -0500

    feat: Add method to fill cars with seed data

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CARS_SEED } from './data/cars.seed';
 import { BRANDS_SEED } from './data/brands.seed';
+import { CarsService } from 'src/cars/cars.service';
 
 @Injectable()
 export class SeedService {
+  constructor(private readonly carsService: CarsService) {}
+
   populateDB() {
     // CARS_SEED
     // BRANDS_SEED
 
     // return CARS_SEED;
     // return BRANDS_SEED;
+
+    this.carsService.fillCarsWithSeedData(CARS_SEED);
 
     return 'Seed executed successfully!';
   }
