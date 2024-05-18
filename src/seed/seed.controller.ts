a86ebfde931aa6b774ad5d4e48e9ea39d468c9cf
ceosmore <ceosmore@gmail.com>
2024-05-18 11:00:09 -0500

    Refactor SeedController to remove unused methods and code

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -18,16 +18,6 @@ export class SeedController {
     return this.seedService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.seedService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSeedDto: UpdateSeedDto) {
-    return this.seedService.update(+id, updateSeedDto);
-  }
-
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.seedService.remove(+id);
